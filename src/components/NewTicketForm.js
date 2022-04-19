b21771b99d3b20877e73f509d2b73c489e3dd061
Ryan Ashby <rjapsu160@gmail.com>
2022-04-19 14:45:01 -0700

    add actual form to NewTicketForm.js

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { v4 } from 'uuid'; // new code
 
 function NewTicketForm(props){
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewTicketFormSubmission}>
+        <input
+          type='text'
+          name='names'
+          placeholder='Pair Names' />
+        <input
+          type='text'
+          name='location'
+          placeholder='Location' />
+        <textarea
+          name='issue'
+          placeholder='Describe your issue.' />
+        <button type='submit'>Request Help</button>
+      </form>
     </React.Fragment>
   );
 }
