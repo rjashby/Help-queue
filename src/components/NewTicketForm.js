8c89a32d5208b8486cd2fa03d72ff5f7de6a1b0a
Ryan Ashby <rjapsu160@gmail.com>
2022-04-21 08:02:42 -0700

    add TicketDetail.js

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { v4 } from 'uuid'; // new code
+import PropTypes from "prop-types"; //import PropTypes
 
 function NewTicketForm(props){
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4()});
+  }
+
   return (
     <React.Fragment>
       <form onSubmit={handleNewTicketFormSubmission}>
@@ -21,5 +28,10 @@ function NewTicketForm(props){
     </React.Fragment>
   );
 }
+
+// We also need to add PropTypes for our new prop.
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,7 +7,9 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      mainTicketList: [], // new code
+      selectedTicket: null //new code
     };
   }
 
@@ -17,15 +19,23 @@ class TicketControl extends React.Component {
     }));
   }
 
+  handleAddingNewTicketToList = (newTicket) => {
+    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+    this.setState({
+      mainTicketList: newMainTicketList,
+      formVisibleOnPage: false 
+      });
+  }
+
   render(){
     let currentlyVisibleState = null;
     let buttonText = null; // new code
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />;
-      buttonText = "Return to Ticket List"; // new code
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />  // new code in this line 
+      buttonText = "Return to Ticket List";
     } else {
-      currentlyVisibleState = <TicketList />;
-      buttonText = "Add Ticket"; // new code
+      currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList} />; // new code
+      buttonText = "Add Ticket"; 
     }
     return (
       <React.Fragment>
